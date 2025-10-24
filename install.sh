47c1d9273265f6e3a983d10953b28e9d00c5d780
hide3110 <fengshi3110@hotmail.com>
2025-10-24 13:53:10 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -134,7 +134,7 @@ After=network.target
 [Service]
 Type=simple
 LimitNOFILE=32768
-ExecStart=/usr/bin/ssservice server --log-without-time -c /etc/shadowsocks-rust/config.json
+ExecStart=ExecStart=/usr/bin/ssserver -c /etc/shadowsocks-rust/config.json
 Restart=on-failure
 
 [Install]
